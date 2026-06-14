9985f77f776af9e2a56adb510961a675057059bd
IDGS-801-23000940 <rcj.pabloo@gmail.com>
2026-06-14 14:40:57 -0600

    feature/SPIA-40-remoto-autenticacion-api-dto

---
+++ app/src/main/java/org/utl/idgs901/space_ai_mobile/data/remote/api/AuthApi.kt
@@ -0,0 +1,14 @@
+package org.utl.idgs901.space_ai_mobile.data.remote.api
+
+import org.utl.idgs901.space_ai_mobile.data.remote.dto.LoginRequestDto
+import org.utl.idgs901.space_ai_mobile.data.remote.dto.LoginResponseDto
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface AuthApi {
+
+    @POST("auth/login")
+    suspend fun login(
+        @Body request: LoginRequestDto
+    ): LoginResponseDto
+}
---
+++ app/src/main/java/org/utl/idgs901/space_ai_mobile/data/remote/dto/AuthDtos.kt
@@ -0,0 +1,23 @@
+package org.utl.idgs901.space_ai_mobile.data.remote.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class LoginRequestDto(
+    @SerializedName("email") val email: String,
+    @SerializedName("password") val password: String
+)
+
+data class LoginResponseDto(
+    @SerializedName("accessToken") val accessToken: String,
+    @SerializedName("refreshToken") val refreshToken: String,
+    @SerializedName("expiresIn") val expiresIn: Long,
+    @SerializedName("user") val user: UserDto
+)
+
+data class UserDto(
+    @SerializedName("id") val id: String,
+    @SerializedName("name") val name: String,
+    @SerializedName("email") val email: String,
+    @SerializedName("folio") val folio: String?,
+    @SerializedName("role") val role: String
+)
