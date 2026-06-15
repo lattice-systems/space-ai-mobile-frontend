7876f347bea9e5dde4c9cf271022ea93ad4b6679
IDGS-801-23000940 <rcj.pabloo@gmail.com>
2026-06-14 21:45:42 -0600

    feature/SPIA-43-limpieza-api-constants

--- app/src/main/java/org/utl/idgs901/space_ai_mobile/core/network/ApiConstants.kt
+++ app/src/main/java/org/utl/idgs901/space_ai_mobile/core/network/ApiConstants.kt
@@ -1,7 +1,6 @@
 package org.utl.idgs901.space_ai_mobile.core.network
 
 object ApiConstants {
-    // In a real project, this would come from BuildConfig
-    const val BASE_URL = "http://10.0.2.2:5000/api/" // Default local emulator URL for ASP.NET
+    const val BASE_URL = "http://10.0.2.2:5000/api/"
     const val TIMEOUT_SECONDS = 30L
 }
