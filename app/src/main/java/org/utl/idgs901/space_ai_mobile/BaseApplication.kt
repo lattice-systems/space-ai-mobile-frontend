18434669dd2f55cf28ce3de18d1982f4e8505601
IDGS-801-23000940 <rcj.pabloo@gmail.com>
2026-06-26 18:02:44 -0600

    config: initialize MapLibre in BaseApplication

--- app/src/main/java/org/utl/idgs901/space_ai_mobile/BaseApplication.kt
+++ app/src/main/java/org/utl/idgs901/space_ai_mobile/BaseApplication.kt
@@ -2,6 +2,12 @@ package org.utl.idgs901.space_ai_mobile
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import org.maplibre.android.MapLibre
 
 @HiltAndroidApp
-class BaseApplication : Application()
+class BaseApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        MapLibre.getInstance(this)
+    }
+}
