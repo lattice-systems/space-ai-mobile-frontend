04cc7ea113dc5214f64bcb506f52ce2c6400f667
IDGS-801-23000940 <rcj.pabloo@gmail.com>
2026-06-26 18:02:42 -0600

    data(map): add MapAssetDataSource for asset reading

---
+++ app/src/main/java/org/utl/idgs901/space_ai_mobile/data/map/datasource/MapAssetDataSource.kt
@@ -0,0 +1,13 @@
+package org.utl.idgs901.space_ai_mobile.data.map.datasource
+
+import android.content.Context
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+
+class MapAssetDataSource @Inject constructor(
+    @ApplicationContext private val context: Context
+) {
+    fun readAsset(fileName: String): String {
+        return context.assets.open(fileName).bufferedReader().use { it.readText() }
+    }
+}
