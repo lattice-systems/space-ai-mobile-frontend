db55bd1bad08f37fceb0f9a70a54cf2aba483f7d
IDGS-801-23000940 <rcj.pabloo@gmail.com>
2026-06-29 13:31:58 -0600

    chore: add navigation-compose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.compose.material3.windowSizeClass)
     implementation(libs.material)
     implementation(libs.androidx.material.icons.extended)
