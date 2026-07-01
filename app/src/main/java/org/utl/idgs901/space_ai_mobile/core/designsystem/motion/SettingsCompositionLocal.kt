26f3e0afa75598589aed69274fb03d955554fc5f
IDGS-801-23000940 <rcj.pabloo@gmail.com>
2026-06-30 19:19:50 -0600

    feat: setup Proto DataStore for user settings persistence

---
+++ app/src/main/java/org/utl/idgs901/space_ai_mobile/core/designsystem/motion/SettingsCompositionLocal.kt
@@ -0,0 +1,6 @@
+package org.utl.idgs901.space_ai_mobile.core.designsystem.motion
+
+import androidx.compose.runtime.staticCompositionLocalOf
+import org.utl.idgs901.space_ai_mobile.domain.settings.model.SettingsPreferences
+
+val LocalSettingsPreferences = staticCompositionLocalOf { SettingsPreferences() }
